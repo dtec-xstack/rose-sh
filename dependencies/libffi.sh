c3afb92294035a6ff192c12fcdf75c1ad8d63717
Justin Too <doubleotoo@gmail.com>
2014-01-17 15:35:30 -0800

    Update libffi to download from website mirrors

--- dependencies/libffi.sh
+++ dependencies/libffi.sh
@@ -1,14 +1,10 @@
-# NOTE: Requires Python.  Should we include a controlled version of python?
-#       Use --with-python=/opt/stonesoup/... if desired.
-
 : ${LIBFFI_DEPENDENCIES:=}
 : ${LIBFFI_CONFIGURE_OPTIONS:=
     --prefix="${ROSE_SH_DEPS_PREFIX}"
     --exec-prefix="${ROSE_SH_DEPS_PREFIX}"
     --libdir="${ROSE_SH_DEPS_PREFIX}"
   }
-: ${LIBFFI_TARBALL:="${DEPENDENCIES_DIR}/libffi-3.0.13.tar.gz"}
-: ${LIBFFI_SOURCE_DIRNAME:="libffi-3.0.13"}
+: ${LIBFFI_TARBALL:="libffi-3.0.13.tar.gz"}
 : ${LIBFFI_INSTALLED_FILE:="${ROSE_SH_DEPS_PREFIX}/lib/libffi-3.0.13/include/ffi.h"}
 
 #-------------------------------------------------------------------------------
@@ -32,11 +28,13 @@ install_libffi()
   set -x
   #-----------------------------------------------------------------------------
   if [ ! -f "${LIBFFI_INSTALLED_FILE}" ]; then
+      rm -rf "./libffi"  || fail "Unable to remove old application workspace"
       mkdir -p "libffi"  || fail "Unable to create application workspace"
       cd "libffi/"       || fail "Unable to change into the application workspace"
 
-      tar xzvf "${LIBFFI_TARBALL}"    || fail "Unable to unpack application tarball"
-      cd "${LIBFFI_SOURCE_DIRNAME}"   || fail "Unable to change into application source directory"
+      download_tarball "${LIBFFI_TARBALL}"    || fail "Unable to download application tarball"
+      tar xzvf "${LIBFFI_TARBALL}"            || fail "Unable to unpack application tarball"
+      cd "${LIBFFI_TARBALL%.tar.gz}"          || fail "Unable to change into application source directory"
 
       ./configure ${LIBFFI_CONFIGURE_OPTIONS} || fail "Unable to configure application"
 
