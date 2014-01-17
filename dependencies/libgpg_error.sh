6d2b5d9d77169939410f799f73424a6c0db1e369
Justin Too <doubleotoo@gmail.com>
2014-01-17 14:59:23 -0800

    Update libgpg_error to download from website mirrors

--- dependencies/libgpg_error.sh
+++ dependencies/libgpg_error.sh
@@ -1,4 +1,11 @@
 : ${LIBGPG_ERROR_DEPENDENCIES:=}
+: ${LIBGPG_ERROR_CONFIGURE_OPTIONS:=
+    --prefix="${ROSE_SH_DEPS_PREFIX}"
+    --libdir="${ROSE_SH_DEPS_LIBDIR}"
+    --enable-static
+  }
+: ${LIBGPG_ERROR_TARBALL:="libgpg-error-1.12.tar.gz"}
+: ${LIBGPG_ERROR_INSTALLED_FILE:="${ROSE_SH_DEPS_PREFIX}/include/gpg-error.h"}
 
 #-------------------------------------------------------------------------------
 install_libgpg_error()
@@ -20,23 +27,19 @@ install_libgpg_error()
   #-----------------------------------------------------------------------------
   set -x
   #-----------------------------------------------------------------------------
-  if [ ! -f "${ROSE_SH_DEPS_PREFIX}/include/gpg-error.h" ]; then
-      mkdir -p "libgpg_error"  || exit 1
-      cd "libgpg_error/"    || exit 1
+  if [ ! -f "${LIBGPG_ERROR_INSTALLED_FILE}" ]; then
+      rm -rf "./libgpg_error"                           || fail "Unable to create application workspace"
+      mkdir -p "libgpg_error"                           || fail "Unable to create application workspace"
+      cd "libgpg_error/"                                || fail "Unable to change into the application workspace"
 
-      tar xzvf "${DEPENDENCIES_DIR}/libgpg-error-1.12.tar.gz" || exit 1
-      cd "libgpg-error-1.12/" || exit 1
+      download_tarball "${LIBGPG_ERROR_TARBALL}"        || fail "Unable to download application tarball"
+      tar xzvf "${LIBGPG_ERROR_TARBALL}"                || fail "Unable to unpack application tarball"
+      cd "$(basename ${LIBGPG_ERROR_TARBALL%.tar.gz})"  || fail "Unable to change into application source directory"
 
-      ./configure \
-          --prefix="$ROSE_SH_DEPS_PREFIX" \
-          --libdir="$ROSE_SH_DEPS_LIBDIR" \
-          \
-              --enable-static \
-          \
-          || exit 1
+      ./configure ${LIBGPG_ERROR_CONFIGURE_OPTIONS}     || fail "Unable to configure application"
 
-      make -j${parallelism} || exit 1
-      make -j${parallelism} install || exit 1
+      make -j${parallelism}                     || fail "An error occurred during application compilation"
+      make -j${parallelism} install             || fail "An error occurred during application installation"
   else
       info "[SKIP] libgpg_error is already installed"
   fi
