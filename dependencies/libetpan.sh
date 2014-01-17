b7093ef4a0bdf66d6763d26bd03d04d413705aeb
Justin Too <doubleotoo@gmail.com>
2014-01-17 15:11:06 -0800

    Update libetpan to download from website mirrors

--- dependencies/libetpan.sh
+++ dependencies/libetpan.sh
@@ -1,4 +1,15 @@
 : ${LIBETPAN_DEPENDENCIES:=openssl zlib cyrus_sasl db expat curl}
+: ${LIBETPAN_CONFIGURE_OPTIONS:=
+    --prefix="${ROSE_SH_DEPS_PREFIX}"
+    --libdir="${ROSE_SH_DEPS_LIBDIR}"
+    --with-openssl="$ROSE_SH_DEPS_PREFIX"
+    --with-sasl="$ROSE_SH_DEPS_PREFIX"
+    --with-curl="$ROSE_SH_DEPS_PREFIX"
+    --with-expat="$ROSE_SH_DEPS_PREFIX"
+    --with-zlib
+  }
+: ${LIBETPAN_TARBALL:="libetpan-1.2.tar.bz2"}
+: ${LIBETPAN_INSTALLED_FILE:="${ROSE_SH_DEPS_PREFIX}/include/libetpan.h"}
 
 #-------------------------------------------------------------------------------
 install_libetpan()
@@ -20,27 +31,19 @@ install_libetpan()
   #-----------------------------------------------------------------------------
   set -x
   #-----------------------------------------------------------------------------
-  if [ ! -f "${ROSE_SH_DEPS_PREFIX}/include/libetpan.h" ]; then
-      mkdir -p "libetpan"  || exit 1
-      cd "libetpan/"    || exit 1
+  if [ ! -f "${LIBETPAN_INSTALLED_FILE}" ]; then
+      rm -rf "./libetpan"                           || fail "Unable to create application workspace"
+      mkdir -p "libetpan"                           || fail "Unable to create application workspace"
+      cd "libetpan/"                                || fail "Unable to change into the application workspace"
 
-      tar xjvf "${DEPENDENCIES_DIR}/libetpan-1.2.tar.bz2" || exit 1
-      cd "libetpan-1.2/" || exit 1
+      download_tarball "${LIBETPAN_TARBALL}"        || fail "Unable to download application tarball"
+      tar xjvf "${LIBETPAN_TARBALL}"                || fail "Unable to unpack application tarball"
+      cd "$(basename ${LIBETPAN_TARBALL%.tar.bz2})" || fail "Unable to change into application source directory"
 
-      ./configure \
-          --prefix="$ROSE_SH_DEPS_PREFIX" \
-          --libdir="$ROSE_SH_DEPS_LIBDIR" \
-          \
-              --with-openssl="${ROSE_SH_DEPS_PREFIX}" \
-              --with-sasl="${ROSE_SH_DEPS_PREFIX}" \
-              --with-curl="${ROSE_SH_DEPS_PREFIX}" \
-              --with-expat="${ROSE_SH_DEPS_PREFIX}" \
-              --with-zlib \
-          \
-          || exit 1
+      ./configure ${LIBETPAN_CONFIGURE_OPTIONS}     || fail "Unable to configure application"
 
-      make -j${parallelism} || exit 1
-      make -j${parallelism} install || exit 1
+      make -j${parallelism}                     || fail "An error occurred during application compilation"
+      make -j${parallelism} install             || fail "An error occurred during application installation"
   else
       info "[SKIP] libetpan is already installed"
   fi
