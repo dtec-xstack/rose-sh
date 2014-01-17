4aad6ed5d35f30967be8d792ca86d4c06cc8891b
Justin Too <doubleotoo@gmail.com>
2014-01-17 15:53:39 -0800

    Update libcroco to download from website mirrors

--- dependencies/libcroco.sh
+++ dependencies/libcroco.sh
@@ -2,8 +2,7 @@
 : ${LIBCROCO_CONFIGURE_OPTIONS:=
     --prefix="${ROSE_SH_DEPS_PREFIX}"
     --libdir="${ROSE_SH_DEPS_LIBDIR}"}
-: ${LIBCROCO_TARBALL:="${DEPENDENCIES_DIR}/libcroco-0.6.8.tar.xz"}
-: ${LIBCROCO_SOURCE_DIRNAME:="libcroco-0.6.8"}
+: ${LIBCROCO_TARBALL:="libcroco-0.6.8.tar.xz"}
 : ${LIBCROCO_INSTALLED_FILE:="${ROSE_SH_DEPS_PREFIX}/include/libcroco-0.6/libcroco/libcroco.h"}
 
 #-------------------------------------------------------------------------------
@@ -27,12 +26,14 @@ install_libcroco()
   set -x
   #-----------------------------------------------------------------------------
   if [ ! -f "${LIBCROCO_INSTALLED_FILE}" ]; then
+      rm -rf "./libcroco"  || fail "Unable to remove old application workspace"
       mkdir -p "libcroco"  || fail "Unable to create application workspace"
       cd "libcroco/"       || fail "Unable to change into the application workspace"
 
-      unxz "${LIBCROCO_TARBALL}"        || fail "Unable to unpack application tarball"
-      tar xvf "${LIBCROCO_TARBALL%.xz}" || fail "Unable to unpack application tarball"
-      cd "${LIBCROCO_SOURCE_DIRNAME}"   || fail "Unable to change into application source directory"
+      download_tarball "${LIBCROCO_TARBALL}"  || fail "Unable to download application tarball"
+      unxz "${LIBCROCO_TARBALL}"              || fail "Unable to unpack application tarball"
+      tar xvf "${LIBCROCO_TARBALL%.xz}"       || fail "Unable to unpack application tarball"
+      cd "${LIBCROCO_TARBALL%.tar.xz}"        || fail "Unable to change into application source directory"
 
       ./configure ${LIBCROCO_CONFIGURE_OPTIONS} || fail "Unable to configure application"
 
