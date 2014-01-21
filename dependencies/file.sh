119ada3bdb09338389fdf02313343b46549ca040
Justin Too <doubleotoo@gmail.com>
2014-01-21 14:29:23 -0800

    Update file to download from website mirrors

--- dependencies/file.sh
+++ dependencies/file.sh
@@ -4,7 +4,7 @@
     --libdir="${ROSE_SH_DEPS_LIBDIR}"
     --enable-static
   }
-: ${FILE_TARBALL:="${DEPENDENCIES_DIR}/file-5.14.tar.gz"}
+: ${FILE_TARBALL:="file-5.14.tar.gz"}
 : ${FILE_INSTALLED_FILE:="${ROSE_SH_DEPS_PREFIX}/bin/file"}
 
 #-------------------------------------------------------------------------------
@@ -28,16 +28,18 @@ install_file()
   set -x
   #-----------------------------------------------------------------------------
   if [ ! -f "${FILE_INSTALLED_FILE}" ]; then
-      mkdir -p "file"  || fail "Unable to create application workspace"
-      cd "file/"       || fail "Unable to change into the application workspace"
+      rm -rf "./file"                           || fail "Unable to remove old application workspace"
+      mkdir -p "file"                           || fail "Unable to create application workspace"
+      cd "file/"                                || fail "Unable to change into the application workspace"
 
-      tar xzvf "${FILE_TARBALL}"               || fail "Unable to unpack application tarball"
-      cd "$(basename ${FILE_TARBALL%.tar.gz})" || fail "Unable to change into application source directory"
+      download_tarball "${FILE_TARBALL}"        || fail "Unable to download application tarball"
+      tar xzvf "${FILE_TARBALL}"                || fail "Unable to unpack application tarball"
+      cd "$(basename ${FILE_TARBALL%.tar.gz})"  || fail "Unable to change into application source directory"
 
-      ./configure ${FILE_CONFIGURE_OPTIONS} || fail "Unable to configure application"
+      ./configure ${FILE_CONFIGURE_OPTIONS}     || fail "Unable to configure application"
 
-      make -j${parallelism}         || fail "An error occurred during application compilation"
-      make -j${parallelism} install || fail "An error occurred during application installation"
+      make -j${parallelism}                     || fail "An error occurred during application compilation"
+      make -j${parallelism} install             || fail "An error occurred during application installation"
   else
       info "[SKIP] file is already installed"
   fi
