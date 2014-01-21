557047d20706410d844cb8dae0712666106c5302
Justin Too <doubleotoo@gmail.com>
2014-01-21 14:30:49 -0800

    Update libtool to download from website mirrors

--- dependencies/libtool.sh
+++ dependencies/libtool.sh
@@ -4,7 +4,7 @@
     --exec-prefix="${ROSE_SH_DEPS_PREFIX}"
     --libdir="${ROSE_SH_DEPS_LIBDIR}"
   }
-: ${LIBTOOL_TARBALL:="${DEPENDENCIES_DIR}/libtool-2.4.tar.gz"}
+: ${LIBTOOL_TARBALL:="libtool-2.4.tar.gz"}
 : ${LIBTOOL_INSTALLED_FILE:="${ROSE_SH_DEPS_PREFIX}/bin/libtool"}
 
 #-------------------------------------------------------------------------------
@@ -28,16 +28,18 @@ install_libtool()
   set -x
   #-----------------------------------------------------------------------------
   if [ ! -f "${LIBTOOL_INSTALLED_FILE}" ]; then
-      mkdir -p "libtool"  || fail "Unable to create application workspace"
-      cd "libtool/"       || fail "Unable to change into the application workspace"
+      rm -rf "./libtool"                           || fail "Unable to remove old application workspace"
+      mkdir -p "libtool"                           || fail "Unable to create application workspace"
+      cd "libtool/"                                || fail "Unable to change into the application workspace"
 
-      tar xzvf "${LIBTOOL_TARBALL}"               || fail "Unable to unpack application tarball"
-      cd "$(basename ${LIBTOOL_TARBALL%.tar.gz})" || fail "Unable to change into application source directory"
+      download_tarball "${LIBTOOL_TARBALL}"        || fail "Unable to download application tarball"
+      tar xzvf "${LIBTOOL_TARBALL}"                || fail "Unable to unpack application tarball"
+      cd "$(basename ${LIBTOOL_TARBALL%.tar.gz})"  || fail "Unable to change into application source directory"
 
-      ./configure ${LIBTOOL_CONFIGURE_OPTIONS} || fail "Unable to configure application"
+      ./configure ${LIBTOOL_CONFIGURE_OPTIONS}     || fail "Unable to configure application"
 
-      make -j${parallelism}         || fail "An error occurred during application compilation"
-      make -j${parallelism} install || fail "An error occurred during application installation"
+      make -j${parallelism}                     || fail "An error occurred during application compilation"
+      make -j${parallelism} install             || fail "An error occurred during application installation"
   else
       info "[SKIP] libtool is already installed"
   fi
