6e0cf1a96a216fdbfee35dc5fb3db08766ebb009
Justin Too <doubleotoo@gmail.com>
2014-01-28 13:38:58 -0800

    (Applications) (Jedit) Add initial content

---
+++ applications/jedit.sh
@@ -0,0 +1,81 @@
+: ${JEDIT_DEPENDENCIES:=apache_ant ss_ant_rose}
+: ${JEDIT_CONFIGURE_OPTIONS:=
+    -Dcom.pontetec.rosecompiler.use_single_commandline="true"
+  }
+: ${JEDIT_ANT_TARGET:=build}
+
+#-------------------------------------------------------------------------------
+download_jedit()
+#-------------------------------------------------------------------------------
+{
+  info "Downloading source code"
+
+  set -x
+      clone_repository "${application}" "${application}-src" || exit 1
+      cd "${application}-src/" || exit 1
+  set +x
+}
+
+#-------------------------------------------------------------------------------
+install_deps_jedit()
+#-------------------------------------------------------------------------------
+{
+  install_deps ${JEDIT_DEPENDENCIES} || fail "Could not install dependencies"
+}
+
+#-------------------------------------------------------------------------------
+patch_jedit()
+#-------------------------------------------------------------------------------
+{
+  info "Patching not required"
+}
+
+#-------------------------------------------------------------------------------
+configure_jedit__rose()
+#-------------------------------------------------------------------------------
+{
+  info "Compiling application for ROSE compiler='${ROSE_CC}'"
+
+  #-----------------------------------------------------------------------------
+  set -x
+  #-----------------------------------------------------------------------------
+      KG__STRIP_PATH="$(pwd)/" \
+      KG__REPORT_FAIL="$(pwd)/rose-fails.txt" \
+      KG__REPORT_PASS="$(pwd)/rose-passes.txt" \
+        /usr/bin/time --format='%E' \
+            ant "${JEDIT_ANT_TARGET}" \
+                -verbose \
+                -lib "${ROSE_SH_DEPS_PREFIX}/lib" \
+                -Dbuild.compiler="com.pontetec.RoseCompilerAdapter" \
+                -Dcom.pontetec.rosecompiler.translator="${ROSE_CC}" \
+                ${JEDIT_CONFIGURE_OPTIONS} \
+        || fail "An error occurred during application compilation"
+  #-----------------------------------------------------------------------------
+  set +x
+  #-----------------------------------------------------------------------------
+}
+
+#-------------------------------------------------------------------------------
+configure_jedit__gcc()
+#-------------------------------------------------------------------------------
+{
+  info "Configuring application for default compiler='${CC}'"
+
+  #-----------------------------------------------------------------------------
+  set -x
+  #-----------------------------------------------------------------------------
+    /usr/bin/time --format='%E' \
+        ant "${JEDIT_ANT_TARGET}" \
+            ${JEDIT_CONFIGURE_OPTIONS} \
+    || fail "An error occurred during application compilation"
+  #-----------------------------------------------------------------------------
+  set +x
+  #-----------------------------------------------------------------------------
+}
+
+#-------------------------------------------------------------------------------
+compile_jedit()
+#-------------------------------------------------------------------------------
+{
+  info "Application compilation was performed during the configure stage"
+}
