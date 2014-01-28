de1146f43c11a0164a8a4d2c6c04a80514ceceb3
Justin Too <doubleotoo@gmail.com>
2014-01-28 14:05:37 -0800

    (Applications) (Signserver) Add initial content

---
+++ applications/signserver.sh
@@ -0,0 +1,81 @@
+: ${SIGNSERVER_DEPENDENCIES:=apache_ant ss_ant_rose}
+: ${SIGNSERVER_CONFIGURE_OPTIONS:=
+    -Dcom.pontetec.rosecompiler.use_single_commandline="true"
+  }
+: ${SIGNSERVER_ANT_TARGET:=build}
+
+#-------------------------------------------------------------------------------
+download_signserver()
+#-------------------------------------------------------------------------------
+{
+  info "Downloading source code"
+
+  set -x
+      clone_repository "${application}" "${application}-src" || exit 1
+      cd "${application}-src/" || exit 1
+  set +x
+}
+
+#-------------------------------------------------------------------------------
+install_deps_signserver()
+#-------------------------------------------------------------------------------
+{
+  install_deps ${SIGNSERVER_DEPENDENCIES} || fail "Could not install dependencies"
+}
+
+#-------------------------------------------------------------------------------
+patch_signserver()
+#-------------------------------------------------------------------------------
+{
+  info "Patching not required"
+}
+
+#-------------------------------------------------------------------------------
+configure_signserver__rose()
+#-------------------------------------------------------------------------------
+{
+  info "Compiling application for ROSE compiler='${ROSE_CC}'"
+
+  #-----------------------------------------------------------------------------
+  set -x
+  #-----------------------------------------------------------------------------
+      KG__STRIP_PATH="$(pwd)/" \
+      KG__REPORT_FAIL="$(pwd)/rose-fails.txt" \
+      KG__REPORT_PASS="$(pwd)/rose-passes.txt" \
+        /usr/bin/time --format='%E' \
+            ant "${SIGNSERVER_ANT_TARGET}" \
+                -verbose \
+                -lib "${ROSE_SH_DEPS_PREFIX}/lib" \
+                -Dbuild.compiler="com.pontetec.RoseCompilerAdapter" \
+                -Dcom.pontetec.rosecompiler.translator="${ROSE_CC}" \
+                ${SIGNSERVER_CONFIGURE_OPTIONS} \
+        || fail "An error occurred during application compilation"
+  #-----------------------------------------------------------------------------
+  set +x
+  #-----------------------------------------------------------------------------
+}
+
+#-------------------------------------------------------------------------------
+configure_signserver__gcc()
+#-------------------------------------------------------------------------------
+{
+  info "Configuring application for default compiler='${CC}'"
+
+  #-----------------------------------------------------------------------------
+  set -x
+  #-----------------------------------------------------------------------------
+    /usr/bin/time --format='%E' \
+        ant "${SIGNSERVER_ANT_TARGET}" \
+            ${SIGNSERVER_CONFIGURE_OPTIONS} \
+    || fail "An error occurred during application compilation"
+  #-----------------------------------------------------------------------------
+  set +x
+  #-----------------------------------------------------------------------------
+}
+
+#-------------------------------------------------------------------------------
+compile_signserver()
+#-------------------------------------------------------------------------------
+{
+  info "Application compilation was performed during the configure stage"
+}
