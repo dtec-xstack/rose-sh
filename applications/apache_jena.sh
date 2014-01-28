5a19ca75127e0d035c419b3f5c34360934b378f4
Justin Too <doubleotoo@gmail.com>
2014-01-28 14:08:51 -0800

    (Applications) (Apache Jena) Add initial content

---
+++ applications/apache_jena.sh
@@ -0,0 +1,85 @@
+: ${APACHE_JENA_DEPENDENCIES:=apache_ant ss_ant_rose}
+: ${APACHE_JENA_CONFIGURE_OPTIONS:=
+    -Dcom.pontetec.rosecompiler.use_single_commandline="true"
+  }
+: ${APACHE_JENA_ANT_TARGET:=compile}
+
+#-------------------------------------------------------------------------------
+download_apache_jena()
+#-------------------------------------------------------------------------------
+{
+  info "Downloading source code"
+
+  set -x
+      clone_repository "${application}" "${application}-src" || exit 1
+      cd "${application}-src/" || exit 1
+  set +x
+}
+
+#-------------------------------------------------------------------------------
+install_deps_apache_jena()
+#-------------------------------------------------------------------------------
+{
+  install_deps ${APACHE_JENA_DEPENDENCIES} || fail "Could not install dependencies"
+}
+
+#-------------------------------------------------------------------------------
+patch_apache_jena()
+#-------------------------------------------------------------------------------
+{
+  info "Patching not required"
+}
+
+#-------------------------------------------------------------------------------
+configure_apache_jena__rose()
+#-------------------------------------------------------------------------------
+{
+  info "Compiling application for ROSE compiler='${ROSE_CC}'"
+
+  #-----------------------------------------------------------------------------
+  set -x
+  #-----------------------------------------------------------------------------
+      mvn ant:ant || fail "Failed to convert Maven build to Ant build using 'mvn ant:ant'"
+
+      KG__STRIP_PATH="$(pwd)/" \
+      KG__REPORT_FAIL="$(pwd)/rose-fails.txt" \
+      KG__REPORT_PASS="$(pwd)/rose-passes.txt" \
+        /usr/bin/time --format='%E' \
+            ant "${APACHE_JENA_ANT_TARGET}" \
+                -verbose \
+                -lib "${ROSE_SH_DEPS_PREFIX}/lib" \
+                -Dbuild.compiler="com.pontetec.RoseCompilerAdapter" \
+                -Dcom.pontetec.rosecompiler.translator="${ROSE_CC}" \
+                ${APACHE_JENA_CONFIGURE_OPTIONS} \
+        || fail "An error occurred during application compilation"
+  #-----------------------------------------------------------------------------
+  set +x
+  #-----------------------------------------------------------------------------
+}
+
+#-------------------------------------------------------------------------------
+configure_apache_jena__gcc()
+#-------------------------------------------------------------------------------
+{
+  info "Configuring application for default compiler='${CC}'"
+
+  #-----------------------------------------------------------------------------
+  set -x
+  #-----------------------------------------------------------------------------
+    mvn ant:ant || fail "Failed to convert Maven build to Ant build using 'mvn ant:ant'"
+
+    /usr/bin/time --format='%E' \
+        ant "${APACHE_JENA_ANT_TARGET}" \
+            ${APACHE_JENA_CONFIGURE_OPTIONS} \
+    || fail "An error occurred during application compilation"
+  #-----------------------------------------------------------------------------
+  set +x
+  #-----------------------------------------------------------------------------
+}
+
+#-------------------------------------------------------------------------------
+compile_apache_jena()
+#-------------------------------------------------------------------------------
+{
+  info "Application compilation was performed during the configure stage"
+}
