35363b3786e356dd6f41f60d3cb6dd242bef999b
Justin Too <doubleotoo@gmail.com>
2014-01-28 14:05:00 -0800

    (Applications) (OpenDS) Add initial content

---
+++ applications/opends.sh
@@ -0,0 +1,81 @@
+: ${OPENDS_DEPENDENCIES:=apache_ant ss_ant_rose}
+: ${OPENDS_CONFIGURE_OPTIONS:=
+    -Dcom.pontetec.rosecompiler.use_single_commandline="true"
+  }
+: ${OPENDS_ANT_TARGET:=compile}
+
+#-------------------------------------------------------------------------------
+download_opends()
+#-------------------------------------------------------------------------------
+{
+  info "Downloading source code"
+
+  set -x
+      clone_repository "${application}" "${application}-src" || exit 1
+      cd "${application}-src/" || exit 1
+  set +x
+}
+
+#-------------------------------------------------------------------------------
+install_deps_opends()
+#-------------------------------------------------------------------------------
+{
+  install_deps ${OPENDS_DEPENDENCIES} || fail "Could not install dependencies"
+}
+
+#-------------------------------------------------------------------------------
+patch_opends()
+#-------------------------------------------------------------------------------
+{
+  info "Patching not required"
+}
+
+#-------------------------------------------------------------------------------
+configure_opends__rose()
+#-------------------------------------------------------------------------------
+{
+  info "Compiling application for ROSE compiler='${ROSE_CC}'"
+
+  #-----------------------------------------------------------------------------
+  set -x
+  #-----------------------------------------------------------------------------
+      KG__STRIP_PATH="$(pwd)/" \
+      KG__REPORT_FAIL="$(pwd)/rose-fails.txt" \
+      KG__REPORT_PASS="$(pwd)/rose-passes.txt" \
+        /usr/bin/time --format='%E' \
+            ant "${OPENDS_ANT_TARGET}" \
+                -verbose \
+                -lib "${ROSE_SH_DEPS_PREFIX}/lib" \
+                -Dbuild.compiler="com.pontetec.RoseCompilerAdapter" \
+                -Dcom.pontetec.rosecompiler.translator="${ROSE_CC}" \
+                ${OPENDS_CONFIGURE_OPTIONS} \
+        || fail "An error occurred during application compilation"
+  #-----------------------------------------------------------------------------
+  set +x
+  #-----------------------------------------------------------------------------
+}
+
+#-------------------------------------------------------------------------------
+configure_opends__gcc()
+#-------------------------------------------------------------------------------
+{
+  info "Configuring application for default compiler='${CC}'"
+
+  #-----------------------------------------------------------------------------
+  set -x
+  #-----------------------------------------------------------------------------
+    /usr/bin/time --format='%E' \
+        ant "${OPENDS_ANT_TARGET}" \
+            ${OPENDS_CONFIGURE_OPTIONS} \
+    || fail "An error occurred during application compilation"
+  #-----------------------------------------------------------------------------
+  set +x
+  #-----------------------------------------------------------------------------
+}
+
+#-------------------------------------------------------------------------------
+compile_opends()
+#-------------------------------------------------------------------------------
+{
+  info "Application compilation was performed during the configure stage"
+}
