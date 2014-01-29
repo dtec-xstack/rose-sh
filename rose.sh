99062407618ff407b633bc7606191aa7a6a7b0fa
Justin Too <doubleotoo@gmail.com>
2014-01-29 14:07:48 -0800

    Add public Jenkins as a tarball mirror

--- rose.sh
+++ rose.sh
@@ -18,6 +18,7 @@ set -e
   rose-dev@rosecompiler1.llnl.gov:rose/c
   }
 : ${TARBALL_MIRROR_URLS:=
+  http://hudson-rose-30:8080/userContent/tarballs/dependencies
   https://rosecompiler1.llnl.gov:8443/jenkins-edg4x/userContent/tarballs/dependencies
   http://portal.nersc.gov/project/dtec/tarballs/dependencies
   https://bitbucket.org/rose-compiler/rose-sh/downloads
