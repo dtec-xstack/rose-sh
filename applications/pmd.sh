9ab6890c432dba8af0360000fd13aa6047bf4a95
Justin Too <doubleotoo@gmail.com>
2014-01-29 12:39:28 -0800

    (Applications) Add Apache PMD

---
+++ applications/pmd.sh
@@ -0,0 +1,82 @@
+: ${PMD_DEPENDENCIES:=apache_ant ss_ant_rose}
+: ${PMD_CONFIGURE_OPTIONS:=
+    -Dcom.pontetec.rosecompiler.use_single_commandline="true"
+    -Dcom.pontetec.rosecompiler.translator.arg.rose.skip_commentsAndDirectives=""
+  }
+: ${PMD_ANT_TARGET:=compile}
+
+#-------------------------------------------------------------------------------
+download_pmd()
+#-------------------------------------------------------------------------------
+{
+  info "Downloading source code"
+
+  set -x
+      clone_repository "${application}" "${application}-src" || exit 1
+      cd "${application}-src/" || exit 1
+  set +x
+}
+
+#-------------------------------------------------------------------------------
+install_deps_pmd()
+#-------------------------------------------------------------------------------
+{
+  install_deps ${PMD_DEPENDENCIES} || fail "Could not install dependencies"
+}
+
+#-------------------------------------------------------------------------------
+patch_pmd()
+#-------------------------------------------------------------------------------
+{
+  info "Patching not required"
+}
+
+#-------------------------------------------------------------------------------
+configure_pmd__rose()
+#-------------------------------------------------------------------------------
+{
+  info "Compiling application for ROSE compiler='${ROSE_CC}'"
+
+  #-----------------------------------------------------------------------------
+  set -x
+  #-----------------------------------------------------------------------------
+      KG__STRIP_PATH="$(pwd)/" \
+      KG__REPORT_FAIL="$(pwd)/rose-fails.txt" \
+      KG__REPORT_PASS="$(pwd)/rose-passes.txt" \
+        /usr/bin/time --format='%E' \
+            ant "${PMD_ANT_TARGET}" \
+                -verbose \
+                -lib "${ROSE_SH_DEPS_PREFIX}/lib" \
+                -Dbuild.compiler="com.pontetec.RoseCompilerAdapter" \
+                -Dcom.pontetec.rosecompiler.translator="${ROSE_CC}" \
+                ${PMD_CONFIGURE_OPTIONS} \
+        || fail "An error occurred during application compilation"
+  #-----------------------------------------------------------------------------
+  set +x
+  #-----------------------------------------------------------------------------
+}
+
+#-------------------------------------------------------------------------------
+configure_pmd__gcc()
+#-------------------------------------------------------------------------------
+{
+  info "Configuring application for default compiler='${CC}'"
+
+  #-----------------------------------------------------------------------------
+  set -x
+  #-----------------------------------------------------------------------------
+    /usr/bin/time --format='%E' \
+        ant "${PMD_ANT_TARGET}" \
+            ${PMD_CONFIGURE_OPTIONS} \
+    || fail "An error occurred during application compilation"
+  #-----------------------------------------------------------------------------
+  set +x
+  #-----------------------------------------------------------------------------
+}
+
+#-------------------------------------------------------------------------------
+compile_pmd()
+#-------------------------------------------------------------------------------
+{
+  info "Application compilation was performed during the configure stage"
+}
